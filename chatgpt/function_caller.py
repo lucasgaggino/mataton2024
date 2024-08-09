d7d09f1276006fb3eb5a13f45a1ed20e5d806d34
Mateo Bertogliati <mateo.bertogliati@iquall.net>
2024-08-09 14:35:47 -0300

    Funciones para troubleshooting + remediation

--- chatgpt/function_caller.py
+++ chatgpt/function_caller.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import random
 
 from tools.tools_functions import get_pc_data
+from tools.tools_functions import *
 
 # Load the API key from the .env file
 
@@ -16,7 +17,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
 # Load the tools from the JSON file
-TOOLS_PATH = "./chatgpt/tools/random.json"
+TOOLS_PATH = "./chatgpt/tools/remediacion-inteligente.json"
 with open(TOOLS_PATH) as f:
     tools = json.load(f)
 
@@ -48,8 +49,17 @@ def run_conversation(content,tools=None):
         messages.append(response_message)
 
         available_functions = {
-            "get_current_weather": get_current_weather,
-            "get_pc_data": get_pc_data,
+            "remediation_action_do_nothing": remediation_action_do_nothing,
+            "remediation_action_restart_interface": remediation_action_restart_interface,
+            "remediation_action_restart_device": remediation_action_restart_device,
+            "remediation_action_change_bsp_configuration": remediation_action_change_bsp_configuration,
+            "get_ip_interface_brief": get_ip_interface_brief,
+            "show_running_config_interface": show_running_config_interface,
+            "show_interfaces": show_interfaces,
+            "show_ip_route": show_ip_route,
+            "ping": ping,
+            "traceroute": traceroute,
+            "show_logging": show_logging
         }
         for tool_call in tool_calls:
             print(f"Function: {tool_call.function.name}")
@@ -57,13 +67,10 @@ def run_conversation(content,tools=None):
             function_name = tool_call.function.name
             function_to_call = available_functions[function_name]
             function_args = json.loads(tool_call.function.arguments)
-            if function_name == "get_current_weather":
+            if function_name in available_functions:
                 function_response = function_to_call(
-                    latitude=function_args.get("latitude"),
-                    longitude=function_args.get("longitude"),
+                        device_name=function_args.get("device_name"),
                     )
-            elif function_name == "get_pc_data":
-                function_response = function_to_call()
             print(f"API: {function_response}")
             messages.append(
             {
@@ -81,9 +88,22 @@ def run_conversation(content,tools=None):
         )
     return second_response
 
+#DATOS PROVISIORIOS
+ESTADO = ["DEGRADATION","AFFECTATION","FATAL EVENT","NO ISSUES"][1]
+SERVICIO = ["INTERNET","TELEFONIA_MOVIL","TELEFONIA_FIJA"][0]
+TARGET = "NE-PALERMO-01"
+HISTORIAL = [
+ 
+    ]
+
 if __name__ == "__main__":
-    question = "Should i wear a winter jacket in Buenos Aires today?"
-    #question= "What's my pc total memory ?"
+
+    question = f"""Your are a smart troubleshooting agent for a CSP. Given troublehsooting information about Cisco routers you will determine which actions to take to remediate the isssue.
+    There has been {ESTADO} in the service {SERVICIO} in the device {TARGET}
+    The device is a {DEFAULT_DEVICE_NAME}, these are the last actions taken in the past: {HISTORIAL}.
+    Given your knowledge and experience, you will determine which REMEDATION ACTION to take and return the command sequence
+    to execute in the target device. You can call as many functions as you need. You can call the troubleshooting functions to get more device info if needed.
+    You must return only the command sequence with no enclosing characters or the literal string NO_ACTION_NEEDED if there is no action to take. You must not return any troubleshooting commands."""
     response = run_conversation(question,tools=tools)
     print(question)
     for chunk in response:
--- chatgpt/tools/tools_functions.py
+++ chatgpt/tools/tools_functions.py
@@ -28,13 +28,14 @@ def get_pc_data():
         result = query_prometheus(query)
         response.append({name:result})
     return json.dumps(response)
-
+    
 DEFAULT_DEVICE_NAME = "Cisco Router Model IOS"
 DEFAULT_BOOT_COMMAND = "flash:/cisco-ios-image.bin" 
 DEFAULT_INTERFACE_NAME = "GigabitEthernet0/1"
+def remediation_action_do_nothing(device_name: str) -> str:
+    return ""
 def remediation_action_restart_interface(device_name: str) -> str:
-    return
-f"""
+    return f"""
 enable
 configure terminal
 interface {DEFAULT_INTERFACE_NAME}
@@ -46,17 +47,108 @@ write memory
 """
 
 def remediation_action_restart_device(device_name: str) -> str:
-    return
-"""
+    return """
 enable
 reload in 0
 """
 def remediation_action_change_bsp_configuration(device_name: str) -> str:
-    return 
-f"""
+    return f"""
 enable
 configure terminal
 boot system {DEFAULT_BOOT_COMMAND}
 exit
 write memory
 """
+def get_ip_interface_brief(device_name: str) -> str:
+    return """
+Interface              IP-Address      OK? Method Status                Protocol
+GigabitEthernet0/0     192.168.1.1     YES manual up                    up
+GigabitEthernet0/1     unassigned      YES unset  administratively down down
+GigabitEthernet0/2     10.0.0.1        YES manual up                    up
+"""
+def show_running_config_interface(device_name: str) -> str:
+    return """
+Building configuration...
+
+Current configuration : 86 bytes
+!
+interface GigabitEthernet0/0
+ip address 192.168.1.1 255.255.255.0
+duplex auto
+speed auto
+end
+"""
+ 
+def show_interfaces(device_name: str) -> str:
+    return """
+GigabitEthernet0/0 is up, line protocol is up
+    Hardware is iGbE, address is 0011.2233.4455 (bia 0011.2233.4455)
+    Internet address is 192.168.1.1/24
+    MTU 1500 bytes, BW 1000000 Kbit/sec, DLY 10 usec,
+        reliability 255/255, txload 1/255, rxload 1/255
+    Encapsulation ARPA, loopback not set
+    Keepalive set (10 sec)
+    Full-duplex, 1Gbps, media type is RJ45
+    output flow-control is XON, input flow-control is XON
+    5 minute input rate 1000 bits/sec, 1 packets/sec
+    5 minute output rate 2000 bits/sec, 2 packets/sec
+        1000 packets input, 200000 bytes, 0 no buffer
+        Received 1000 broadcasts, 0 runts, 0 giants, 0 throttles
+        0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
+        0 watchdog, 0 multicast, 0 pause input
+        0 input packets with dribble condition detected
+        2000 packets output, 400000 bytes, 0 underruns
+        0 output errors, 0 collisions, 0 interface resets
+        0 babbles, 0 late collision, 0 deferred
+        0 lost carrier, 0 no carrier, 0 PAUSE output
+        0 output buffer failures, 0 output buffers swapped out
+"""
+def show_ip_route(device_name: str) -> str:
+    return """
+Codes: C - connected, S - static, R - RIP, M - mobile, B - BGP
+            D - EIGRP, EX - EIGRP external, O - OSPF, IA - OSPF inter area
+            N1 - OSPF NSSA external type 1, N2 - OSPF NSSA external type 2
+            E1 - OSPF external type 1, E2 - OSPF external type 2
+            i - IS-IS, L1 - IS-IS level-1, L2 - IS-IS level-2, ia - IS-IS inter area
+            * - candidate default, U - per-user static route, o - ODR
+            P - periodic downloaded static route, + - replicated route
+
+     Gateway of last resort is not set
+
+     C    192.168.1.0/24 is directly connected, GigabitEthernet0/0
+     O    10.0.0.0/24 [110/2] via 10.0.0.2, 00:00:03, GigabitEthernet0/2
+""" 
+def ping(device_name: str) -> str:
+    return """
+Type escape sequence to abort.
+     Sending 5, 100-byte ICMP Echos to 192.168.1.1, timeout is 2 seconds:
+     !!!!!
+     Success rate is 100 percent (5/5), round-trip min/avg/max = 1/1/2 ms
+"""
+def traceroute(device_name: str) -> str:
+    return """
+Type escape sequence to abort.
+     Tracing the route to 8.8.8.8
+
+      1  192.168.1.254  1 msec  1 msec  1 msec
+      2  10.0.0.2  2 msec  2 msec  2 msec
+      3  172.16.0.1  10 msec  10 msec  10 msec
+      4  *  *  *
+      5  *  *  *
+      6  8.8.8.8  50 msec  50 msec  50 msec
+"""
+def show_logging(device_name: str) -> str:
+    return """
+Syslog logging: enabled (0 messages dropped, 0 flushes, 0 overruns)
+       Console logging: disabled
+       Monitor logging: level debugging, 0 messages logged
+       Buffer logging: level debugging, 20 messages logged
+       Trap logging: level informational, 0 message lines logged
+
+     Log Buffer (4096 bytes):
+     %LINK-3-UPDOWN: Interface GigabitEthernet0/0, changed state to up
+     %LINEPROTO-5-UPDOWN: Line protocol on Interface GigabitEthernet0/0, changed state to up
+     %LINK-3-UPDOWN: Interface GigabitEthernet0/1, changed state to down
+     %LINEPROTO-5-UPDOWN: Line protocol on Interface GigabitEthernet0/1, changed state to down
+"""
+
