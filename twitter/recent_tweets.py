bbf433c48b8aac7dd3ce65556e59e9549bf440b6
@lucas.gaggino1 <lucas.gaggino@gmail.com>
2024-08-08 23:40:11 -0300

    twiitter recent tweets example

---
+++ twitter/recent_tweets.py
@@ -0,0 +1,41 @@
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+
+KEY_PATH = "./keys.env"
+load_dotenv(dotenv_path=KEY_PATH)
+
+bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': '(from:elonmusk)','tweet.fields': 'author_id'}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
--- twitter/test_tweepy.py
+++ twitter/test_tweepy.py
@@ -11,15 +11,14 @@ consumer_secret = os.environ.get("TWITTER_API_SECRET_KEY")
 
 access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
 
 print(access_token)
 client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
-    access_token=access_token, access_token_secret=access_token_secret
+    access_token=access_token, access_token_secret=access_token_secret,bearer_token=bearer_token
 )
 
 
-response = client.create_tweet(
-    text="This Tweet was Tweeted using Tweepy and Twitter API v2!"
-)
+response = client.search_all_tweets(query="python", max_results=10)
 print(f"https://twitter.com/user/status/{response.data['id']}")
